fa597ab9b2a1961e0d97a450a05ac104dfc5899b
Florian Wartner <florian@wartner.io>
2023-06-21 17:11:57 +0200

    Added origins

--- config/middlewares.js
+++ config/middlewares.js
@@ -20,13 +20,14 @@ module.exports = [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", `https://loremflickr.com/`],
+          "img-src": ["'self'", "data:", "blob:", `https://loremflickr.com/`, "https://market-assets.strapi.io", "https://res.cloudinary.com", "*"],
           'media-src': [
             "'self'",
             'data:',
             'blob:',
             'market-assets.strapi.io',
             'res.cloudinary.com',
+            '*'
           ],
           upgradeInsecureRequests: null,
         },
